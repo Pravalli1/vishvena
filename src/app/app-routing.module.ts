c9afa2bb68fd518dc9041eb2f5a7698a04eb2b1c
Pravalli1 <pentakotapravallika6@gmail.com>
2024-12-19 01:12:12 +0530

    vishvena

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,
+    children:[
+      {path:'contact',component:ContactComponent},
+      {path:'expertise',component:ExpertiseComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
